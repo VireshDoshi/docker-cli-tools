ade599f7517d2dd1dba2a428f0aaca6e0517302b
Ric Harvey <ric@ngd.io>
2017-08-22 16:04:00 +0100

    repo prep

--- dockerfiles/aws-shell/get_tags.sh
+++ dockerfiles/aws-shell/get_tags.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
   echo "Getting all releases"
-  curl https://api.github.com/repos/awslabs/aws-shell/tags | jq -r .[].name > releases
-  curl https://api.github.com/repos/awslabs/aws-shell/tags | jq -r .[].name | head -n1 > latest
+  curl https://api.github.com/repos/awslabs/aws-shell/tags | jq -r .[].name | grep -v reinvent-demo > releases
+  curl https://api.github.com/repos/awslabs/aws-shell/tags | jq -r .[].name | grep -v reinvent-demo | head -n1 > latest
