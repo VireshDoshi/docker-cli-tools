8e20f35eee4fd10b2affb9cacfe01207f0548cf6
Ric Harvey <ric@ngd.io>
2017-09-22 09:22:49 +0100

    update build.sh to speed up build time

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export status=`curl -Is https://hub.docker.com/v2/repositories/richarvey/${1}/tags/$(cat dockerfiles/${1}/latest)/ | head -n 1|cut -d$' ' -f2`
+export latest=`curl -Is https://hub.docker.com/v2/repositories/richarvey/${1}/tags/$(cat dockerfiles/${1}/latest)/ | head -n 1|cut -d$' ' -f2`
 
-if [ ${status} == "200" ]; then
+if [ ${latest} == "200" ]; then
     echo "Build Exists: Nothing to do!"
     exit 0
 else
@@ -10,8 +10,13 @@ else
     cd dockerfiles/${1}/
     for rel in `cat releases`; do 
         export VERSION="${rel}" && \
-        docker build --build-arg VERSION="${VERSION}" -t "richarvey/${1}:${VERSION}" . && \
-        docker push "richarvey/${1}:${VERSION}"
+        export status=`curl -Is https://hub.docker.com/v2/repositories/richarvey/${1}/tags/${VERSION}/ | head -n 1|cut -d$' ' -f2` && \
+        if [ $status} == "200" ]; then
+            echo "Build Exists: Nothing to do!"
+        else
+            docker build --build-arg VERSION="${VERSION}" -t "richarvey/${1}:${VERSION}" . && \
+            docker push "richarvey/${1}:${VERSION}"
+        fi
     done
 
     latest=`cat latest` && docker tag richarvey/${1}:${latest} richarvey/${1}:latest && docker push "richarvey/${1}:latest"
